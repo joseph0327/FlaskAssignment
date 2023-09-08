2cdcca47deabf5304707e9ac44707e0350410a81
joseph0327 <joseph.villarin0327@gmail.com>
2023-09-08 15:27:59 +0800

    added answer to assigment

--- phasebook/match.py
+++ phasebook/match.py
@@ -20,8 +20,14 @@ def match(match_id):
 
 
 def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
-            return False
+    #Converting the list to a set for faster membership tests and the removal of duplicate elements
+    set_fave_numbers_1 = set(fave_numbers_1)
+    
+    # Use set intersection to check for matching elements
+    intersection = set_fave_numbers_1.intersection(fave_numbers_2)
+    
+    # If the intersection is not empty, there are matching elements
+    # In Python, an empty set is considered False, and a non-empty set is considered True.
+    return bool(intersection)
 
-    return True
+
--- phasebook/search.py
+++ phasebook/search.py
@@ -1,30 +1,49 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 
 from .data.search_data import USERS
-
 
 bp = Blueprint("search", __name__, url_prefix="/search")
 
 
 @bp.route("")
 def search():
-    return search_users(request.args.to_dict()), 200
+    query_params = request.args.to_dict()
+    matching_users = filter_users(query_params)
+    sorted_users = sort_users(matching_users, query_params)
+    return jsonify({"results": sorted_users}), 200
+
+def filter_users(query_params):
+    filtered_users = []
+
+    for user in USERS:
+        match = False
+
+        if "id" in query_params and user["id"] == query_params["id"]:
+            match = True
+        if "name" in query_params:
+            if query_params["name"].lower() in user["name"].lower():
+                match = True
+        if "age" in query_params and str(user["age"]) == query_params["age"]:
+            match = True
+        if "occupation" in query_params:
+            if query_params["occupation"].lower() in user["occupation"].lower():
+                match = True
+
+        if match:
+            filtered_users.append(user)
+
+    return filtered_users
 
 
-def search_users(args):
-    """Search users database
 
-    Parameters:
-        args: a dictionary containing the following search parameters:
-            id: string
-            name: string
-            age: string
-            occupation: string
+def sort_users(users, query_params):
+    priority_order = ["id", "name", "age", "occupation"]
 
-    Returns:
-        a list of users that match the search parameters
-    """
+    def matching_priority(user):
+        priority_values = [-priority_order.index(field) if query_params.get(field) == user.get(field) else len(priority_order) for field in priority_order]
+        return priority_values
 
-    # Implement search here!
+    sorted_users = sorted(users, key=matching_priority)
 
-    return USERS
+    return sorted_users
+
